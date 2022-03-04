76ccf039c526dc833b1dee8a42d45ea5fd844513
Prakhar <rinayush@gmail.com>
2022-03-04 15:24:35 +0530

    Printing Peak(highest) element in mountain array

---
+++ WORKING with ARRAYS/Imp competitive questions/mountain_array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Highest(int arr[], int n)
+{
+    int h = -1;
+    for (int i = 1; i < (n - 1); i++)
+    {
+
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+        {
+            h = arr[i];
+        }
+    }
+    return h;
+}
+
+int main()
+{
+    int arr[7] = {1, 3, 4, 9, 8, 6, 0};
+    int ans = Highest(arr, 7);
+    cout << ans<< endl;
+    return 0;
+}
